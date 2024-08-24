227da17a1b51ef71cca737277774bf32ab96e9fe
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-08-24 13:31:00 -0500

    Add return value and console.log

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-function getComputerChoice() {
-    let rock = 0;
-    let scissors = 1;
-    let paper = 2;
+//Computer chooses rock, paper, or scissors
+function getComputerChoice(max) {
+    const rock = 0;
+    const scissors = 1;
+    const paper = 2;
+    return Math.floor(Math.random() * max)
 }
+//Print result
+console.log(getComputerChoice(3));
