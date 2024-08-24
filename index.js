a66a21bb00017ddcbddcd9806e39c4991e9f8faa
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-08-24 13:49:56 -0500

    Revise getComputerChoice function
    Replace rock paper scissors variables with an array called choices that include rock paper scissors
    Add variable randomIndex. This randomizes the computer's choice.
    Revise return so that it returns choices using randomIndex

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //Computer chooses rock, paper, or scissors
 function getComputerChoice(max) {
-    const rock = 0;
-    const scissors = 1;
-    const paper = 2;
-    return Math.floor(Math.random() * max)
+    const choices = ["rock", "paper", "scissors"]
+    const randomIndex = Math.floor(Math.random() * max);
+    return choices[randomIndex]
 }
 //Print result
 console.log(getComputerChoice(3));
