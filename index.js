69a3a3ece98c5330ef260df15a0baf07feed122f
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2024-08-24 15:02:46 -0500

    Finish function playRound

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getHumanChoice() {
     return userChoice.toLowerCase()
 }
 //Print results
-console.log(getHumanChoice())
+console.log(getHumanChoice(""))
 
 
 //Keep track of scores from players and computer
@@ -23,4 +23,23 @@ let computerScore = 0
 
 
 //Play a round
-function playRound( humanChoice , computerChoice );
+function playRound(humanChoice, computerChoice) {
+
+    //Define which choice beats which
+    const outcomes = {
+        rock: "scissors",
+        paper: "rock",
+        scissors: "paper"
+    };
+
+    //Determine winner of this round
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    } else if (outcomes[humanChoice] === computerChoice) {
+        ++humanScore //Increase player's score
+        console.log("You win!")
+    } else {
+        ++computerScore //Increase computer's score
+        console.log("You lose. Better luck next time!")
+    }   
+};
